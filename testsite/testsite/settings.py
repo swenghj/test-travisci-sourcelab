f66dd37c31d6b32852bc9567770dfa57ff62006b
joonjo <hyunjoon.jo.922@gmail.com>
2017-01-12 13:32:45 -0800

    ALLOWED_HOSTS

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -94,6 +94,12 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
