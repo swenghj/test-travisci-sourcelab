ab63a4c541b94a0248508a4f650e09169ad27399
joonjo <hyunjoon.jo.922@gmail.com>
2017-03-07 14:12:43 -0800

    ap test

---
+++ selenium/automation/ap-demo-regression.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2.7
+
+#--------------------------------------------------------------------
+# 
+# Title: ap-demo-regression.py - wrapper file for running all scripts
+#
+#--------------------------------------------------------------------
+
+from subprocess import call
+
+# list of the running scripts
+#scripts = [
+#    "demo-travisci-heroku-1.py",
+#    "demo-travisci-heroku-2.py"
+#]
+
+scripts = []
+
+if __name__ == '__main__':
+    try:
+        testfile = None
+        for script in scripts:
+            testfile = script
+            call("python %s  -i travisci -d sauce" % script, shell=True)
+       
+    except Exception as e:
+        print e
+        raise Exception("Cannot run %s: " % testfile, e)
+
